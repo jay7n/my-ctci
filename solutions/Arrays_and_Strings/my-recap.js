356d52287798d14b32362fd9815bb8a9e31f8098
jay7n <lookof@gmail.com>
2022-02-20 23:57:08 +0800

    add review for Array_and_String[WIP] x2

--- solutions/Arrays_and_Strings/my-recap.js
+++ solutions/Arrays_and_Strings/my-recap.js
@@ -4,9 +4,9 @@
  * can be found for a given problem, where we can use Random-Access, we should take
  * advantage of that chance without hesitation.
  * 
- * From my POV, Hash-Table is a pretty efficient DS that applys the idea of Array's
- * Random-Access. Behind the scene such a DS sees everything in the world as an 
- * ORDERED WAY, or everything can be seen in such an ordered way. If arbitrary elements
+ * Hash-Table is a pretty efficient DS. Behind the scene such a DS sees everything 
+ * in this world as an ORDERED WAY, or everything can be seen in such an ordered way. 
+ * From my POV it applys the idea of Array's Random-Access. If arbitrary elements
  * has a chance to be sorted in some manner, then they can be stored in Array, and
  * Random-Access is hence able to be employed to improve the computing efficiency.
  */
