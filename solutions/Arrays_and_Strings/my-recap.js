a54ed025f540a15f24fa147a3c981b9cf8efb759
jay7n <lookof@gmail.com>
2022-02-23 22:42:37 +0800

    add review for Array_and_String[WIP] x4

--- solutions/Arrays_and_Strings/my-recap.js
+++ solutions/Arrays_and_Strings/my-recap.js
@@ -1,6 +1,6 @@
 /**
  * ## Array, Random-Access, HashTable and Lookup Table.
- * 
+ *
  * One very important feature of Array is Random-Access, which allows us to
  * directly access any arbitrary element in a sequence in the constant time. If
  * any chance can be found for a given problem, where we can use Random-Access,
@@ -17,7 +17,21 @@
  * been sorted automatically. Then we can create a fixed length array to store
  * them (using their code as the index) as a lookup table, which is pretty
  * useful when we need to lookup them later again for some reason in a problem.
- * the lookup cost is fairly cheap and constant as it's from Array's Random-Access.
- * 
+ * the lookup cost is fairly cheap and constant as it's from Array's
+ * Random-Access.
+ *
  * In CTCI questions such as 1_1, 1_2, 1_4 are all cases of this kind of idea. 
+ *
+ *
+ * ## String Traverse, Immediate Return and Fixed Length Array
+ * 
+ * According to the nature of the given question, we do not need
+ * HashTable(Lookup Table) or Random-Access. What we need is to traverse the
+ * string or array element by element. Such a traversal is inevitable since the
+ * question's nature requests it. In the process of traversal we can check if
+ * one element hits some condition, where usually we can get the conclusion then
+ * and immediately return. An immediate return won't change the computing cost
+ * from the Big-O perspective, but it's still worth the optimization for small
+ * questions（as the question is small, you should do it more precisely I think）.
+ *
  */
