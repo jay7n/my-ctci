b4b11e0227b207bf78b182bf41b29cb8440f0597
jay7n <lookof@gmail.com>
2022-02-26 15:55:13 +0800

    finished 2_1 solution2(follow up requirements) x2

--- solutions/Linked_List/2_1/solution2.js
+++ solutions/Linked_List/2_1/solution2.js
@@ -1,3 +1,10 @@
+/** this solution is aim for the follow-up requirement of the question in which
+ * condition a temporary buffer is not allowed to use.
+ *
+ * remarks: though we face a follow-up requirement, the solution is more like a
+ * time-space tradeoff, rather than a better and upgraded solution idea.
+ * 
+ */ 
 module.exports = function RemoveDups(linkedList) {
 	let anchor = linkedList.head;
 	while(anchor) {
