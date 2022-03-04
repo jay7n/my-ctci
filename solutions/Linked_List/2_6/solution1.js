517555cb2a20a18ebd0e4f931a64ebfe0d32b29e
jay7n <lookof@gmail.com>
2022-03-04 11:13:29 +0800

    finished 2_6 solution1

--- solutions/Linked_List/2_6/solution1.js
+++ solutions/Linked_List/2_6/solution1.js
@@ -1,2 +1,41 @@
 module.exports = function Palindrome(linkedList) {
+	let res = false;
+	let frunner = null;
+	let shouldgoon = true;
+	let len = 0;
+	let counter = 0;
+
+	function recursive(node, cb1, cb2) {
+		if (node) {
+			len++;
+			recursive(node.next, cb1, cb2);
+			cb1(node);
+		} else {
+			cb2();
+		}
+	}
+
+	function frunninghalf() {
+		return counter >= len / 2;
+	}
+
+	recursive(linkedList.head, (nodeReverseOrder) => {
+		if (shouldgoon == true) {
+			res = nodeReverseOrder.data == frunner.data;
+			console.log('b =', nodeReverseOrder.data, 'f = ', frunner.data, 'res = ', res);
+			if (frunninghalf() == false) {
+				frunner = frunner.next;
+				counter++;
+			} else {
+				shouldgoon = false;
+			}
+		}
+
+	}, () => {
+		frunner = linkedList.head;
+		counter++;
+	});
+
+
+	return res;
 }
--- solutions/Linked_List/2_6/test.js
+++ solutions/Linked_List/2_6/test.js
@@ -6,7 +6,31 @@ const testWithSolution = function(title, solution) {
 		const linkedList = new LinkedList([1,2,3,2,1]);
 		const isPalindrome = solution(linkedList);
 		expect(isPalindrome).toBe(true);
-	})
+	});
+	test(title + ': ' + 
+	'linkedList [1,2,3,3,2,1] is a palindrome', () => {
+		const linkedList = new LinkedList([1,2,3,3,2,1]);
+		const isPalindrome = solution(linkedList);
+		expect(isPalindrome).toBe(true);
+	});
+	test(title + ': ' + 
+	'linkedList [1,2,3,4,2,1] is not a palindrome', () => {
+		const linkedList = new LinkedList([1,2,3,4,2,1]);
+		const isPalindrome = solution(linkedList);
+		expect(isPalindrome).toBe(false);
+	});
+	test(title + ': ' + 
+	'linkedList [1,2] is not a palindrome', () => {
+		const linkedList = new LinkedList([1,2]);
+		const isPalindrome = solution(linkedList);
+		expect(isPalindrome).toBe(false);
+	});
+	test(title + ': ' + 
+	'linkedList [1] is a palindrome', () => {
+		const linkedList = new LinkedList([1]);
+		const isPalindrome = solution(linkedList);
+		expect(isPalindrome).toBe(true);
+	});
 }
 
 const Solution1 = require('./solution1');
