058dd82af3d9c53f6e877baeb007586d63af37f1
jay7n <lookof@gmail.com>
2022-03-11 01:18:28 +0800

    finished 3_6 solution1

--- solutions/Stacks_and_Queues/3_6/solution1.js
+++ solutions/Stacks_and_Queues/3_6/solution1.js
@@ -1,20 +1,56 @@
 const { LinkedList, LinkedListNode } = require('../../../datastructures/LinkedList/solution1');
 
 class AnimalShelter {
-	enqueue()	{
+	list;
 
+	constructor() {
+		this.list = new LinkedList();
+	}
+
+	enqueue(type, value)	{
+		this.list.add([type, value]);
 	}
 
 	dequeueAny() {
-
+		if (this.list.isEmpty()) {
+			return null;
+		}
+		const [type, value] = this.list.removeNode(this.list.head).data;
+		return {
+			type,
+			value
+		};
 	}
 
 	dequeueDog() {
-
+		return this.dequeueByType('Dog');
 	}
 
 	dequeueCat() {
+		return this.dequeueByType('Cat');
+	}
 
+	dequeueByType(Type) {
+		if (this.list.isEmpty()) {
+			return null;
+		}
+		let cur = this.list.head;
+		while(cur) {
+			if (cur.data[0] == Type) {
+				break;
+			}
+			cur = cur.next;
+		}
+
+		if (cur == null) {
+			return null
+		};
+
+		const [type, value] = this.list.removeNode(cur).data;
+		return {
+			type,
+			value
+		}
 	}
 }
 
