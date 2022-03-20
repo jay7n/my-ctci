5ef6fc6419bd56fe6c8839b5f98c4d9b87e744ba
jay7n <lookof@gmail.com>
2022-03-20 15:47:40 +0800

    created 4_7

---
+++ solutions/Trees_and_Graphs/4_7/test.js
@@ -0,0 +1,14 @@
+const testWithSolution = function(title, solution) {
+	test(title + ': ' + 
+	'', () => {
+		const input = {
+			project: ['a', 'b', 'c', 'd', 'e', 'f'],
+			dependencies: {'a': 'd', 'f': 'b', 'b': 'd', 'f': 'a', 'd': 'c'}
+		}
+		const output = solution(input);
+		expect(output).toStrictEqual(['f', 'e', 'a', 'b', 'd', 'c']);
+	});
+}
+
+const Solution = require('./solution1');
+testWithSolution('Solution1', Solution);
