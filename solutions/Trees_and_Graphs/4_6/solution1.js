10140f39ae390b3484e228bc95ae93fab24d83e5
jay7n <lookof@gmail.com>
2022-03-20 13:19:51 +0800

    finished 4_6 solution1

--- solutions/Trees_and_Graphs/4_6/solution1.js
+++ solutions/Trees_and_Graphs/4_6/solution1.js
@@ -1,3 +1,22 @@
 module.exports = function Successor(treeNode) {
-
+	if (treeNode) {
+		if (treeNode.right) { // if treeNode has right branch, the successor is the leftmost leaf of the right branch
+			let cur = treeNode.right;
+			while(cur.left) {
+				cur = cur.left;
+			}
+			return cur;
+		} else if (treeNode.parent && treeNode.parent.left == treeNode) { // or else if treeNode is the left child of its parent, then the successor is the parent
+			return treeNode.parent;
+		} else if (treeNode.parent && treeNode.parent.right == treeNode) { // or else if treeNode is the right child of its parent, then the successor is the rightmost parent when traversing along the tree up
+			let cur = treeNode.parent;
+			while(cur.parent && cur.parent.right == cur) {
+				cur = cur.parent;
+			}
+			if (cur.parent && cur.parent.left == cur) {
+				return cur.parent;
+			}
+		}
+	}
+	return null;
 }
