9ed9581a5b1d3dd6e4593c448c22e2cce9b3ac23
jay7n <lookof@gmail.com>
2022-03-21 20:11:58 +0800

    creating 4_8

---
+++ solutions/Trees_and_Graphs/4_8/solution1.js
@@ -0,0 +1,3 @@
+module.exports = function CommonAncestor(treeNode1, treeNode2) {
+
+}
---
+++ solutions/Trees_and_Graphs/4_8/test.js
@@ -0,0 +1,9 @@
+const testWithSoution = function (title, solution) {
+	test(title + ': ' +
+	'', () => {
+
+	});
+}
+
+const Solution1 = require('./solution1');
+testWithSoution('Solution1', Solution1);
