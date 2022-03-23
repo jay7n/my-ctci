20f4e7120a3d0e53e306dc8a78b61b24e3f0a23c
jay7n <lookof@gmail.com>
2022-03-23 22:47:45 +0800

    implementing 4_9 solution1

--- solutions/Trees_and_Graphs/4_9/solution1.js
+++ solutions/Trees_and_Graphs/4_9/solution1.js
@@ -1,5 +1,11 @@
 module.exports = function BSTSequences(treeNode) {
-
+	function postOrderTraverse(tNode, cb) {
+		if (tNode) {
+			postOrderTraverse(tNode.left, cb);
+			postOrderTraverse(tNode.right, cb)
+			cb(tNode);
+		}
+	}
 
 	return [
 		[2,1,3],
