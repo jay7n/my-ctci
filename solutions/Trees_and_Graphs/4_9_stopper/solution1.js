9116fc985ba9a1bb4580c0564d09e3501edf6fbc
jay7n <lookof@gmail.com>
2022-03-27 16:12:50 +0800

    implementing 4_9 solution1

--- solutions/Trees_and_Graphs/4_9_stopper/solution1.js
+++ solutions/Trees_and_Graphs/4_9_stopper/solution1.js
@@ -3,7 +3,7 @@ module.exports = class BSTSequences {
 	rootNode;
 
 	constructor(rootNode) {
-		this.rootNode = tNode;
+		this.rootNode = rootNode;
 	}
 
 	getTNodeSeq(tNode) {
@@ -41,24 +41,28 @@ module.exports = class BSTSequences {
 
 	static GetWeavedArrays(array1, array2) {
 		if (array1 == null || array1.length == 0) {
-			return [array2];
+			return [array2.slice()]; // point1 - should copy array, or else the original array would be modified at external level
 		} else if (array2 == null || array2.length == 0) {
-			return [array1];
+			return [array1.slice()]; // point1 - should copy array, or else the original array would be modified at external level
 		}
 
-		const array1Head = array1.splice(0)[0];
+		const array1Head = array1.splice(0,1)[0];
+		// console.log('array1 = ', array1);
 		let res1 = BSTSequences.GetWeavedArrays(array1, array2);
 		res1.map(arr => {
 			return arr.splice(0,0,array1Head);
 		});
+		// console.log('res1 = ', res1);
 
 		array1.splice(0,0,array1Head);
 
-		const array2Head = array2.splice(0)[0];
+		const array2Head = array2.splice(0,1)[0];
+		// console.log('array2 = ', array2);
 		let res2 = BSTSequences.GetWeavedArrays(array1, array2);
 		res2.map(arr => {
 			return arr.splice(0,0,array2Head);
 		});
+		// console.log('res2 = ', res2);
 
 		array2.splice(0,0,array2Head);
 
--- solutions/Trees_and_Graphs/4_9_stopper/test.js
+++ solutions/Trees_and_Graphs/4_9_stopper/test.js
@@ -5,24 +5,31 @@ const testWithSolution = function(title, BSTSequences) {
 		let array1 = [1,2];
 		let array2 = [3,4];
 		const weavedArrays = BSTSequences.GetWeavedArrays(array1, array2);
-		console.log('weavedArrays = ', weavedArrays);
+		expect(weavedArrays).toStrictEqual([
+		  [ 1, 2, 3, 4 ],
+      [ 1, 3, 2, 4 ],
+      [ 1, 3, 4, 2 ],
+      [ 3, 1, 2, 4 ],
+      [ 3, 1, 4, 2 ],
+      [ 3, 4, 1, 2 ]
+		]);
 	});
 
-	// test(title + ': ' + 
-	// 'test solution getTNodeSeq', () => {
-	// 	const root = new BinaryTreeNode(2);
-	// 	root.addLeftChildByData(1);
-	// 	root.addLeftChildByData(3);
+	test(title + ': ' + 
+	'test solution getTNodeSeq', () => {
+		const root = new BinaryTreeNode(2);
+		root.addLeftChildByData(1);
+		root.addLeftChildByData(3);
 
-	// 	const bstSeq = new BSTSequences(root);
+		const bstSeq = new BSTSequences(root);
 
-	// 	bstSeq.getTNodeSeq(root);
+		bstSeq.getTNodeSeq(root);
 
-	// 	// const allArrays = bstSeq.getAllSequences().map(arr => arr.join());
-	// 	// allArrays.includes('2,1,3');
-	// 	// allArrays.includes('2,3,1');
+		// const allArrays = bstSeq.getAllSequences().map(arr => arr.join());
+		// allArrays.includes('2,1,3');
+		// allArrays.includes('2,3,1');
 
-	// });
+	});
 }
 
 const Solution1 = require('./solution1');
