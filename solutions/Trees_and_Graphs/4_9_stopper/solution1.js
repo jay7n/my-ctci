cc4263da2404c15e963bc2b8c2b5d03499723406
jay7n <lookof@gmail.com>
2022-03-27 12:46:33 +0800

    implementing 4_9 solution1

--- solutions/Trees_and_Graphs/4_9_stopper/solution1.js
+++ solutions/Trees_and_Graphs/4_9_stopper/solution1.js
@@ -6,12 +6,46 @@ module.exports = class BSTSequences {
 		this.rootNode = tNode;
 	}
 
-	postOrderTraverse(tNode, cb) {
+	postOrderTraverse(tNode) {
 		if (tNode) {
-			postOrderTraverse(tNode.left, cb);
-			postOrderTraverse(tNode.right, cb)
-			cb(tNode);
+			if (tNode.left) {
+				postOrderTraverse(tNode.left);
+			}
+			if (tNode.right) {
+				postOrderTraverse(tNode.right);
+			}
+
+			if (tNode.left == null && tNode.right == null) { // this node is a leaf
+
+			} else {
+
+			}
 		}
+	}
+
+	getTNodeSeq(tNode) {
+		if (tNode) {
+			let resArrays = [];
+
+			let leftArrays = this.getTNodeSeq(tNode.left);
+			let rightArrays = this.getTNodeSeq(tNode.right);
+
+			if (leftArrays && rightArrays) {
+				for (const la of leftArrays) {
+					for (const ra of rightArrays) {
+						resArrays = resArrays.concat(BSTSequences.GetWeavedArrays(la, ra));
+					}
+				}
+				resArrays = resArrays.map(array => array.splice(0,0,tNode.data));
+			} else if (leftArrays) {
+				resArrays = leftArrays.map(array => array.splice(0,0,tNode.data));
+			} else if (rightArrays) {
+				resArrays = rightArrays.map(array => array.splice(0,0,tNode.data));
+			} else {
+				return [ [tNode.data] ];
+			}
+		}
+		return null;
 	}
 
 	static GetWeavedArrays(array1, array2) {
