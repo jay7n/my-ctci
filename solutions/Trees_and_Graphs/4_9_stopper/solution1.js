6d6491bad18dd6d0612938ff6afc1707fa4357c9
jay7n <lookof@gmail.com>
2022-03-27 15:46:06 +0800

    implementing 4_9 solution1

--- solutions/Trees_and_Graphs/4_9_stopper/solution1.js
+++ solutions/Trees_and_Graphs/4_9_stopper/solution1.js
@@ -25,21 +25,44 @@ module.exports = class BSTSequences {
 			} else if (rightArrays) {
 				resArrays = rightArrays.map(array => array.splice(0,0,tNode.data));
 			} else {
-				return [ [tNode.data] ];
+				resArrays = [ [tNode.data] ];
 			}
+
+			console.log('resArrays = ', resArrays);
 		}
 		return null;
 	}
-
-	static GetWeavedArrays(array1, array2) {
-
-	}
-
 
 	getAllSequences() {
 		const res = this.getTNodeSeq(this.rootNode);
 		console.log('getAllSequences = ', res);
 		return res;
+	}
+
+	static GetWeavedArrays(array1, array2) {
+		if (array1 == null || array1.length == 0) {
+			return [array2];
+		} else if (array2 == null || array2.length == 0) {
+			return [array1];
+		}
+
+		const array1Head = array1.splice(0)[0];
+		let res1 = BSTSequences.GetWeavedArrays(array1, array2);
+		res1.map(arr => {
+			return arr.splice(0,0,array1Head);
+		});
+
+		array1.splice(0,0,array1Head);
+
+		const array2Head = array2.splice(0)[0];
+		let res2 = BSTSequences.GetWeavedArrays(array1, array2);
+		res2.map(arr => {
+			return arr.splice(0,0,array2Head);
+		});
+
+		array2.splice(0,0,array2Head);
+
+		return [].concat(res1, res2);
 	}
 
 }
--- solutions/Trees_and_Graphs/4_9_stopper/test.js
+++ solutions/Trees_and_Graphs/4_9_stopper/test.js
@@ -1,26 +1,28 @@
 const { BinaryTreeNode } = require('../../../datastructures/Tree/solution1');
 const testWithSolution = function(title, BSTSequences) {
 	test(title + ': ' +
-	'test solution part1', () => {
+	'test solution GetWeavedArrays', () => {
 		let array1 = [1,2];
-		let array2 = [3,4]
+		let array2 = [3,4];
 		const weavedArrays = BSTSequences.GetWeavedArrays(array1, array2);
 		console.log('weavedArrays = ', weavedArrays);
 	});
 
-	test(title + ': ' + 
-	'test solution part2', () => {
-		const root = new BinaryTreeNode(2);
-		root.addLeftChildByData(1);
-		root.addLeftChildByData(3);
+	// test(title + ': ' + 
+	// 'test solution getTNodeSeq', () => {
+	// 	const root = new BinaryTreeNode(2);
+	// 	root.addLeftChildByData(1);
+	// 	root.addLeftChildByData(3);
 
-		const bstSeq = new BSTSequences(root);
+	// 	const bstSeq = new BSTSequences(root);
 
-		const allArrays = bstSeq.getAllSequences().map(arr => arr.join());
-		allArrays.includes('2,1,3');
-		allArrays.includes('2,3,1');
+	// 	bstSeq.getTNodeSeq(root);
 
-	});
+	// 	// const allArrays = bstSeq.getAllSequences().map(arr => arr.join());
+	// 	// allArrays.includes('2,1,3');
+	// 	// allArrays.includes('2,3,1');
+
+	// });
 }
 
 const Solution1 = require('./solution1');
