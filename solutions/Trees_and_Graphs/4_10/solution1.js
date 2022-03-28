2e42c8389385fb01f81becb16b9ad96430aa6c0e
jay7n <lookof@gmail.com>
2022-03-29 05:04:07 +0800

    implementing 4_10 solution1

--- solutions/Trees_and_Graphs/4_10/solution1.js
+++ solutions/Trees_and_Graphs/4_10/solution1.js
@@ -1,3 +1,26 @@
 module.exports = function CheckSubTree(T2, T1) {
+	function identicalTrees(t1, t2) {
 
+	}
+
+	function preOrderBFSTraverse(tNode, cb) {
+		let array = [tNode];
+		while(array.length > 0) {
+			const node = array.splice(0, 1)[0];
+			if (node) {
+				cb(node);
+				array.push(node.left);
+				array.push(node.right);
+			}
+		}
+	}
+
+	let isIdentical = false;
+	preOrderBFSTraverse(T1, (nodeInT1) => {
+		if (isIdentical !== true) {
+			isIdentical = identicalTrees(nodeInT1, T2);
+		}
+	});
+
+	return isIdentical;
 }
