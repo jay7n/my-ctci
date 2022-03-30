e04c1c175c3e41f5c10edc11f4b8f24e73a15a98
jay7n <lookof@gmail.com>
2022-03-30 23:22:59 +0800

    creating 4_11

---
+++ solutions/Trees_and_Graphs/4_11/solution1.js
@@ -0,0 +1,5 @@
+class RandomNode {
+
+}
+
+module.exports = RandomNode;
---
+++ solutions/Trees_and_Graphs/4_11/test.js
@@ -0,0 +1,6 @@
+const testWithSolution = function(title, solution) {
+
+}
+
+const Solution1 = require('./solution1');
+testWithSolution('Solution1', Solution1);
