12b7c380c3103d573bf92de44f6bd282271a89da
jay7n <lookof@gmail.com>
2022-04-01 23:06:30 +0800

    creating testcase for 4_11

--- solutions/Trees_and_Graphs/4_11_stopper/test.js
+++ solutions/Trees_and_Graphs/4_11_stopper/test.js
@@ -1,5 +1,16 @@
-const testWithSolution = function(title, solution) {
+const { BinaryTreeNode } = require('../../../datastructures/Tree/solution1');
 
+const testWithSolution = function(title, solution) {
+  test(title + ':' +
+  '', function() {
+		const root = new BinaryTreeNode(10);
+    const n20 = root.addLeftChildByData(20);
+    const n30 = root.addRightChildByData(30);
+    const n40 = n20.addLeftChildByData(30);
+    const n50 = n20.addRightChildByData(30);
+    const n60 = n30.addLeftChildByData(60);
+    const n70 = n30.addLeftChildByData(60);
+  });
 }
 
 const Solution1 = require('./solution1');
