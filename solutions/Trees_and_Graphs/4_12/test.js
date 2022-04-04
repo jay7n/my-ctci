c1592b2f8a09ff55dbbc0291bf0c88f11aa1ece6
jay7n <lookof@gmail.com>
2022-04-04 22:32:28 +0800

    creating 4_12

---
+++ solutions/Trees_and_Graphs/4_12/test.js
@@ -0,0 +1,11 @@
+const { BinaryTreeNode } = require('../../../datastructures/Tree/solution1');
+
+const testWithSolution = function(title, solution) {
+  test(title + ': ' + 
+  '', () => {
+
+  });
+}
+
+const Solution1 = require('./solution1');
+testWithSolution('Solution1', Solution1);
