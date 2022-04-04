4e344083cad482469d0161926db8aabafee3e5a3
jay7n <lookof@gmail.com>
2022-04-04 16:15:39 +0800

    implementing 4_11 solution1

--- solutions/Trees_and_Graphs/4_11_stopper/solution1.js
+++ solutions/Trees_and_Graphs/4_11_stopper/solution1.js
@@ -3,19 +3,29 @@ class RandomNode {
   right;
   parent;
   data;
+  leftCount;
+  rightCount;
 
   constructor(data) {
     this.data = data;
+    this.leftCount = 0;
+    this.rightCount = 0;
+  }
+
+  get count() {
+    return this.leftCount + this.rightCount + 1;
   }
 
   insert(subBranchName, randomNode) {
     if (subBranchName == 'left') {
       this.left = randomNode;
       randomNode.parent = this;
+      this.leftCount = randomNode.count;
       return true;
     } else if (subBranchName == 'right') {
       this.right = randomNode;
       randomNode.parent = this;
+      this.rightCount = randomNode.count;
       return true;
     } else {
       return false;
@@ -46,8 +56,10 @@ class RandomNode {
       const parent = nodeToDelete.parent;
       nodeToDelete.parent = null;
       if (parent.left === nodeToDelete) {
+        parent.leftCount = 0;
         parent.left = null;
       } else {
+        parent.rightCount = 0;
         parent.right = null;
       }
     }
