568b930cf2d5405bda94aa48c21bffb4555047ab
jay7n <lookof@gmail.com>
2022-04-04 13:34:22 +0800

    implementing 4_11 solution1

--- solutions/Trees_and_Graphs/4_11_stopper/solution1.js
+++ solutions/Trees_and_Graphs/4_11_stopper/solution1.js
@@ -1,10 +1,25 @@
 class RandomNode {
-  getRandomNode() {
+  left;
+  right;
+  parent;
+  data;
 
+  constructor(data) {
+    this.data = data;
   }
 
-  insert() {
-
+  insert(subBranchName, randomNode) {
+    if (subBranchName == 'left') {
+      this.left = randomNode;
+      randomNode.parent = this;
+      return true;
+    } else if (subBranchName == 'right') {
+      this.right = randomNode;
+      randomNode.parent = this;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   find() {
@@ -14,6 +29,11 @@ class RandomNode {
   delete() {
 
   }
+
+  getRandomNode() {
+
+  }
+
 }
 
 module.exports = RandomNode;
