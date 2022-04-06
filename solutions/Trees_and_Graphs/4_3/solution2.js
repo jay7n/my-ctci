f324bf212653fca02e5e348ed73dac2ad032b56a
jay7n <lookof@gmail.com>
2022-04-06 22:48:47 +0800

    finished 4_3 solution2

--- solutions/Trees_and_Graphs/4_3/solution2.js
+++ solutions/Trees_and_Graphs/4_3/solution2.js
@@ -1,14 +1,25 @@
 const { LinkedList } = require('../../../datastructures/LinkedList/solution1');
 
 module.exports = function ListOfDepths(rootTreeNode) {
+  const resLists = [];
 
   function preOrderDfsTraverse(tNode, depth) {
     if (tNode) {
-      ;
+      if (resLists[depth] == null) {
+        resLists[depth] = new LinkedList();
+      }
+
+      const list = resLists[depth];
+      list.add(tNode.data);
+
       preOrderDfsTraverse(tNode.left, depth + 1);
       preOrderDfsTraverse(tNode.right, depth + 1);
     }
   }
 
   preOrderDfsTraverse(rootTreeNode, 0);
+
+  // console.log('resLists = ', resLists.map(list => list.toArray()));
+
+  return resLists.map(list => list.toArray());
 }
