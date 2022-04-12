5ed8f3e1239a4cf607a780e9c24460164208823b
jay7n <lookof@gmail.com>
2022-04-12 21:25:53 +0800

    implementing 4_7 solution2

--- solutions/Trees_and_Graphs/4_7_stopper/solution2.js
+++ solutions/Trees_and_Graphs/4_7_stopper/solution2.js
@@ -6,4 +6,57 @@ module.exports = function BuildOrder(input) {
 		return [];
 	}
 
+  const pg = buildGraph(projects, dependencies);
+
+}
+
+function buildGraph(projects, dependencies) {
+  const pg = new ProjGraph();
+  projects.forEach(name => {
+    const gNode = new ProjGraphNode(name);
+    pg.addProjNode(gNode);
+  });
+  dependencies.forEach(({onName, depName}) => {
+    const onNode = pg.depsMap[onName];
+    const depNode = pg.depsMap[depName];
+    pg.addDependency(depNode, onNode)
+  });
+  return pg;
+}
+
+
+class ProjGraph {
+  nodes;
+  nodesMap;
+  
+  constructor() {
+    this.nodes = [];
+    this.nodesMap = {};
+  }
+
+  addProjNode(gNode) {
+    this.nodes.push(gNode);
+    this.nodesMap[gNode.name] = gNode;
+  }
+
+  addDependency(depNode, onNode) {
+    onNode.addDependencyOnMe(depNode);
+  }
+}
+
+class ProjGraphNode {
+  name;
+  deps; // list of dependencies on me
+  depsMap;
+
+  constructor(name) {
+    this.name = name;
+    this.deps = [];
+    this.depsMap = {};
+  }
+
+  addDependencyOnMe(gNode) {
+    this.deps.push(gNode);
+    this.depsMap[gNode.name] = gNode;
+  }
 }
