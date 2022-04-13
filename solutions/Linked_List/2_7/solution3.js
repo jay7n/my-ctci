3b63ea08c80bcfedb183d29edd88506e4cc22075
jay7n <lookof@gmail.com>
2022-04-13 21:58:39 +0800

    finished 2_7 solution3

--- solutions/Linked_List/2_7/solution3.js
+++ solutions/Linked_List/2_7/solution3.js
@@ -1,2 +1,30 @@
 module.exports = function Intersection(linkedList1, linkedList2) {
+  const l1Length = linkedList1.toArray().length;
+  const l2Length = linkedList2.toArray().length;
+
+  // chop off the exceeded nodes of the longer list
+  let distance = 0;
+  let longerList;
+  if (l1Length > l2Length) {
+    distance = l1Length - l2Length;
+    longerList = linkedList1;
+  } else {
+    distance = l2Length - l1Length;
+    longerList = linkedList2;
+  }
+
+  for (let i = 0; i < distance; i++) {
+    longerList.removeNodeAt(i);
+  }
+
+  // now they have the same length
+  let cur1 = linkedList1.head;
+  let cur2 = linkedList2.head;
+
+  while (cur1 !== cur2) {
+    cur1 = cur1.next;
+    cur2 = cur2.next;
+  }
+
+  return cur1;
 }
