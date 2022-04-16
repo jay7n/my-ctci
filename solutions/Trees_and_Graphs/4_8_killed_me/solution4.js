2bb5c7d9450a2d23da2f84229e9e60d517e0db29
jay7n <lookof@gmail.com>
2022-04-16 13:28:52 +0800

    implementing 4_8 solution4

--- solutions/Trees_and_Graphs/4_8_killed_me/solution4.js
+++ solutions/Trees_and_Graphs/4_8_killed_me/solution4.js
@@ -1,3 +1,8 @@
+// This is an optimized version of solution3
+// Instead of taking a brutal-force search in coverInNode() every time,
+// We can start the search from bottom upwards using postInOrder-DFS pattern
+// so that only one full scale traversing could find the truth
 
 module.exports = function CommonAncestor(rootNode, treeNode1, treeNode2) {
+
 }
