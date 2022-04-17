e6bfa2d74f2f53a05e02900e29becc56c94a539d
jay7n <lookof@gmail.com>
2022-04-17 20:26:13 +0800

    finished 4_12 solution2

--- solutions/Trees_and_Graphs/4_12/solution2.js
+++ solutions/Trees_and_Graphs/4_12/solution2.js
@@ -3,28 +3,34 @@
 // list all possible paths. Instead, just knowing the total
 // count is a way to go.
 
-module.exports = function PathsWithSum(tNode, sum) {
-let totalPathCount = 0;
-let hashMap = {};
+module.exports = function PathsWithSum(tNode, givenSum) {
+  let totalPathCount = 0;
+  let hashMap = {};
 
-function preOrderDFSTraverse(tNode, runningSum = 0) {
-  if (tNode) {
-    runningSum += tNode.data;
-    if (hashMap[runningSum] == null) {
-      hashMap[runningSum] = 0;
+  function preOrderDFSTraverse(tNode, runningSum = 0) {
+    if (tNode) {
+      runningSum += tNode.data;
+      if (hashMap[runningSum] == null) {
+        hashMap[runningSum] = 0;
+      }
+      hashMap[runningSum]++;
+
+      // console.log('hashMap = ', hashMap);
+
+      if (runningSum === givenSum) { // point1 - remember to check runningSum itself
+        totalPathCount++;
+      }
+
+      if (hashMap[runningSum - givenSum] > 0) { // point2 - it's 'runningSum - givenSum' not 'givenSum - runningSum' (current runningSum - some sum in hashMap = givenSum)
+        totalPathCount++;
+      }
+
+      preOrderDFSTraverse(tNode.left, runningSum);
+      preOrderDFSTraverse(tNode.right, runningSum);
+
+      hashMap[runningSum]--;
     }
-    hashMap[runningSum]++;
-
-    if (hasMap[sum - runningSum] > 0) {
-      totalPathCount++;
-    }
-
-    preOrderDFSTraverse(tNode.left, runningSum);
-    preOrderDFSTraverse(tNode.right, runningSum);
-
-    hashMap[runningSum]--;
   }
-}
 
   preOrderDFSTraverse(tNode);
 
