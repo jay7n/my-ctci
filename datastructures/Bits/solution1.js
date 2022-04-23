9717ce60322c60afdab49a1e3054b4955adef792
jay7n <lookof@gmail.com>
2022-04-23 11:42:20 +0800

    implementing Bits solution1 (done for positive num)

--- datastructures/Bits/solution1.js
+++ datastructures/Bits/solution1.js
@@ -1,8 +1,8 @@
 const { Stack } = require('../Stack/solution1');
 module.exports = {
-  printNumAs4Bits(num) {
+  printNumAsNBits(num, n = 4) {
     if (num == 0) {
-      return '0';
+      return new Array(n).fill(0).join('');
     }
 
     let st = new Stack;
@@ -13,11 +13,24 @@ module.exports = {
       while (quotient > 0) {
         remainder = quotient % 2;
         quotient = Math.floor(quotient / 2);
-        console.log('q = ', quotient, 'r = ', remainder);
+        // console.log('q = ', quotient, 'r = ', remainder);
         st.push(remainder);
       }
-      const res = st.popToArray().join('');
-      console.log('res = ', res);
+
+      let extraDigits = n - st.listSize;
+      while (extraDigits > 0) {
+        st.push(0);
+        extraDigits--;
+      }
+
+      let res = st.popToArray();
+
+      if (res.length - n > 0) {
+        res = res.splice(res.length - n, num);
+      }
+
+      // console.log('res = ', res.join(''));
+      return res.join('');
     }
   }
 }
--- datastructures/Bits/test.js
+++ datastructures/Bits/test.js
@@ -2,8 +2,31 @@ const Bits = require('./solution1');
 
 const testWithSolution = function(title) {
   test(title + ': ' +
-  'test printIn4Bits', () => {
-    expect(Bits.printNumAs4Bits(8)).toBe('0001');
+  'test printNumAsNBits', () => {
+    expect(Bits.printNumAsNBits(0)).toBe('0000');
+    expect(Bits.printNumAsNBits(1)).toBe('0001');
+    expect(Bits.printNumAsNBits(1, 4)).toBe('0001');
+    expect(Bits.printNumAsNBits(2, 4)).toBe('0010');
+    expect(Bits.printNumAsNBits(3, 4)).toBe('0011');
+    expect(Bits.printNumAsNBits(3, 4)).toBe('0011');
+    expect(Bits.printNumAsNBits(4, 4)).toBe('0100');
+    expect(Bits.printNumAsNBits(5, 4)).toBe('0101');
+    expect(Bits.printNumAsNBits(6, 4)).toBe('0110');
+    expect(Bits.printNumAsNBits(7, 4)).toBe('0111');
+    expect(Bits.printNumAsNBits(8, 4)).toBe('1000');
+    expect(Bits.printNumAsNBits(9, 4)).toBe('1001');
+    expect(Bits.printNumAsNBits(10, 4)).toBe('1010');
+    expect(Bits.printNumAsNBits(11, 4)).toBe('1011');
+    expect(Bits.printNumAsNBits(12, 4)).toBe('1100');
+    expect(Bits.printNumAsNBits(13, 4)).toBe('1101');
+    expect(Bits.printNumAsNBits(14, 4)).toBe('1110');
+    expect(Bits.printNumAsNBits(15, 4)).toBe('1111');
+    expect(Bits.printNumAsNBits(16, 4)).toBe('0000');
+    expect(Bits.printNumAsNBits(17, 4)).toBe('0001');
+
+    expect(Bits.printNumAsNBits(16, 5)).toBe('10000');
+    expect(Bits.printNumAsNBits(17, 5)).toBe('10001');
+
   });
 }
 
