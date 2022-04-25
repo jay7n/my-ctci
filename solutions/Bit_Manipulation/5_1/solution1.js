3e3180cc3f0ac303e87ad1719994c6dcaf4c8966
jay7n <lookof@gmail.com>
2022-04-25 21:59:31 +0800

    implementing 5_1 solution1

--- solutions/Bit_Manipulation/5_1/solution1.js
+++ solutions/Bit_Manipulation/5_1/solution1.js
@@ -1,3 +1,5 @@
+const Bits = require('../../../datastructures/Bits/solution1');
+
 module.exports = function Insertion(N, M, i, j) {
 
 }
--- solutions/Bit_Manipulation/5_1/test.js
+++ solutions/Bit_Manipulation/5_1/test.js
@@ -1,13 +1,18 @@
+const Bits = require('../../../datastructures/Bits/solution1');
+
 const testWithSolution = function(title, solution) {
   test(title + ':' +
   '', () => {
-    const N = 10000000000;
-    const M = 10011;
+    const N = Bits.printBitsAsNum('10000000000');
+    const M = Bits.printBitsAsNum('10011');
+    const expected = Bits.printBitsAsNum('10001001100');
+    console.log('M = ', M, 'N = ', N, 'expected = ', expected);
+
     const i = 2;
     const j = 6;
 
     const res = solution(N, M, i, j);
-    expect(res).toBe(10001001100);
+    expect(res).toBe(expected);
   });
 }
 
