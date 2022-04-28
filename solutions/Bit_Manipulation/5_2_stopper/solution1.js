c3c6e1b5409bc5cc66d3c85d57aa37c6880f1395
jay7n <lookof@gmail.com>
2022-04-28 23:27:05 +0800

    implementing 5_2 solution1

--- solutions/Bit_Manipulation/5_2_stopper/solution1.js
+++ solutions/Bit_Manipulation/5_2_stopper/solution1.js
@@ -1,3 +1,16 @@
 module.exports = function BinaryToString(numVal) {
-
+  let res = [];
+  let cur = numVal;
+  while (cur > 0) {
+    // console.log('cur = ', cur);
+    let elm = 0;
+    cur = cur * 2;
+    if (cur >= 1) {
+      elm = 1;
+      cur -= 1;
+    }
+    res.push(elm);
+  }
+  // console.log('res = ', res);
+  return res.join('');
 }
--- solutions/Bit_Manipulation/5_2_stopper/test.js
+++ solutions/Bit_Manipulation/5_2_stopper/test.js
@@ -1,6 +1,11 @@
 
 const testWithSolution = function(title, solution) {
-
+  test(title + ': ' + 
+  '', () => {
+    expect(solution(0.5)).toBe('1');
+    expect(solution(0.25)).toBe('01');
+    expect(solution(0.125)).toBe('001');
+  });
 }
 
 const Solution1 = require('./solution1');
