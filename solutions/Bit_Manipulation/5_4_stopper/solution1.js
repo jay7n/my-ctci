7fa39342211857661980f6261fdc9b99ddcb2941
jay7n <lookof@gmail.com>
2022-05-04 11:24:17 +0800

    implementing 5_4 solution1

---
+++ solutions/Bit_Manipulation/5_4_stopper/solution1.js
@@ -0,0 +1,5 @@
+const Bits = require('../../../datastructures/Bits/solution1');
+
+module.exports = function NextNumber(num) {
+  const bitsArray = Bits.printNumAsUnsignedBits(num);
+}
--- solutions/Bit_Manipulation/5_4_stopper/test.js
+++ solutions/Bit_Manipulation/5_4_stopper/test.js
@@ -3,7 +3,9 @@ const Bits = require('../../../datastructures/Bits/solution1');
 function testWithSolution (title, solution) {
   test(title + ': ' +
   '', () => {
-    expect(solution(Bits.printBitsAsNum('0111'))).toStrictEqual(['', '01101']);
+    expect(solution(Bits.printBitsAsNum('0101'))).toStrictEqual([
+      Bits.printBitsAsNum('0011'), 
+      Bits.printBitsAsNum('0110')]);
   });
 }
 
