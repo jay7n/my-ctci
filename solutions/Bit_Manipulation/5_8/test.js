f876856f8dcd05c83dbc97af9e684be4afc79fe0
jay7n <lookof@gmail.com>
2022-05-05 12:49:37 +0800

    creating 5_8

---
+++ solutions/Bit_Manipulation/5_8/test.js
@@ -0,0 +1,9 @@
+function testWithSolution(title, solution) {
+  test(title + ':' + 
+  '', () => {
+
+  });
+}
+
+const Solution1 = require('./solution1');
+testWithSolution('Solution1', Solution1);
