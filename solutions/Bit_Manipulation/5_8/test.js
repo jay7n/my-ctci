1593d010e6a454313a5bef69edba23367e143163
jay7n <lookof@gmail.com>
2022-05-06 23:50:36 +0800

    added testcase for 5_8

--- solutions/Bit_Manipulation/5_8/test.js
+++ solutions/Bit_Manipulation/5_8/test.js
@@ -1,7 +1,26 @@
 function testWithSolution(title, solution) {
   test(title + ':' + 
   '', () => {
+    const screenBytes = new ArrayBuffer(8); // 8 * 8 = 64 pixels
+    const res = solution(screenBytes, 16, 4, 12, 1); // width = 16 pixels
+    expect(res).toStrictEqual([
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+  });
 
+  test(title + ':' + 
+  '', () => {
+    const screenBytes = new ArrayBuffer(8); // 8 * 8 = 64 pixels
+    const res = solution(screenBytes, 16, 8, 15, 2); // width = 16 pixels
+    expect(res).toStrictEqual([
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
   });
 }
 
