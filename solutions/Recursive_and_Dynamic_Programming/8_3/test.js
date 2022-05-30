8739397a8735e46f76929829078b69242b7ff680
jay7n <lookof@gmail.com>
2022-05-31 00:23:54 +0800

    adding 8_3

---
+++ solutions/Recursive_and_Dynamic_Programming/8_3/test.js
@@ -0,0 +1,8 @@
+const testWithSolution = function(title, solution) {
+  test(title + '' + 
+  'test case 1', () => {
+  });
+}
+
+const Solution1 = require('./solution1');
+testWithSolution('Solution1', Solution1);
