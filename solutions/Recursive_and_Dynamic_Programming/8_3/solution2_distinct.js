8f85fc0325645b834dd697db10e7533425c6b262
jay7n <lookof@gmail.com>
2022-05-31 21:20:20 +0800

    finished 8_2 with a binary-search algorithm with distinct numbers

--- solutions/Recursive_and_Dynamic_Programming/8_3/solution2_distinct.js
+++ solutions/Recursive_and_Dynamic_Programming/8_3/solution2_distinct.js
@@ -1,3 +1,14 @@
 module.exports = function MagicIndex(array) {
+  function recursive(start, end, array) {
+    const mid = start + Math.floor((end - start) / 2); // point1 - don't remember to add start
+    if (array[mid] > mid) {
+      return recursive(start, mid-1, array);
+    } else if (array[mid] < mid) {
+      return recursive(mid+1, end, array);
+    } else {
+      return mid;
+    }
+  }
 
+  return recursive(0, array.length - 1, array);
 }
