8cbc0e73013c78fc9f97f9994e40494d10c7f586
jay7n <lookof@gmail.com>
2022-05-31 20:04:49 +0800

    created 8_3

--- solutions/Recursive_and_Dynamic_Programming/8_3/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_3/test.js
@@ -1,6 +1,8 @@
 const testWithSolution = function(title, solution) {
   test(title + '' + 
   'test case 1', () => {
+    expect(solution([-5, -2, 0, 2, 4, 10, 20, 100])).toBe(4);
+    expect(solution([-100, -50, -40, -30, -20, -10, 0, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29])).toBe(13);
   });
 }
 
