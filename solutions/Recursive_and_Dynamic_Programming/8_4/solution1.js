44543551ef4e7b42e451f93c5c0bf5d3f426391c
jay7n <lookof@gmail.com>
2022-06-02 22:30:04 +0800

    basically finished 8_4 with a brutal-force solution

--- solutions/Recursive_and_Dynamic_Programming/8_4/solution1.js
+++ solutions/Recursive_and_Dynamic_Programming/8_4/solution1.js
@@ -1,3 +1,16 @@
 module.exports = function PowerSet(array) {
+  function recursive(idx, array) {
+    if (idx > 0) {
+      const res = recursive(idx - 1, array);
+      const newSubSet1 = JSON.parse(JSON.stringify(res));
+      const newSubSet2 = res.map(subSet => {subSet.push(array[idx]); return subSet; });
+      return [].concat(newSubSet1, newSubSet2);
+    } else {
+      return [[], [array[0]]];
+    }
+  }
 
+  const res = recursive(array.length - 1, array);
+  console.log('PowerSet res = ', res);
+  return res;
 }
--- solutions/Recursive_and_Dynamic_Programming/8_4/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_4/test.js
@@ -1,5 +1,11 @@
 const testDistinctWithSolution = function(title, solution) {
-
+  test(title + 
+  '', () => {
+    const subSet = solution([1,2,3]);
+    const expected = [[], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3]];
+    expect(subSet).toEqual(expect.arrayContaining(expected));
+    expect(expected).toEqual(expect.arrayContaining(subSet));
+  });
 }
 
 
