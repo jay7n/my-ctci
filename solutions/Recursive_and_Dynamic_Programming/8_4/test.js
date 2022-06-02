3470285afad4d7d568cc794ed9f563769d970437
jay7n <lookof@gmail.com>
2022-06-02 22:39:10 +0800

    add more test cases for 8_4

--- solutions/Recursive_and_Dynamic_Programming/8_4/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_4/test.js
@@ -1,8 +1,32 @@
 const testDistinctWithSolution = function(title, solution) {
   test(title + 
-  '', () => {
+  'test case 1', () => {
+    const subSet = solution([1,2]);
+    const expected = [
+      [], [1], [2], [1,2], 
+    ];
+    expect(subSet).toEqual(expect.arrayContaining(expected));
+    expect(expected).toEqual(expect.arrayContaining(subSet));
+  });
+  test(title + 
+  'test case 2', () => {
     const subSet = solution([1,2,3]);
-    const expected = [[], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3]];
+    const expected = [
+      [], [1], [2], [3], 
+      [1,2], [1,3], [2,3], [1,2,3]
+    ];
+    expect(subSet).toEqual(expect.arrayContaining(expected));
+    expect(expected).toEqual(expect.arrayContaining(subSet));
+  });
+  test(title + 
+  'test case 3', () => {
+    const subSet = solution([1,2,3,4]);
+    const expected = [
+      [], [1], [2], [3], 
+      [4], [1,2], [1,3], [1,4], 
+      [2,3], [2,4], [3,4],[1,2,3], 
+      [1,2,4], [1,3,4], [2,3,4],[1,2,3,4]
+    ];
     expect(subSet).toEqual(expect.arrayContaining(expected));
     expect(expected).toEqual(expect.arrayContaining(subSet));
   });
