6e6913edba392affe101f4f5efb5a851f402610c
jay7n <lookof@gmail.com>
2022-06-03 18:33:59 +0800

    creating 8_6

---
+++ solutions/Recursive_and_Dynamic_Programming/8_6/test.js
@@ -0,0 +1,9 @@
+const testDistinctWithSolution = function(title, solution) {
+  test(title + 
+  '', () => {
+
+  });
+}
+
+const Solution1 = require('./solution1');
+testDistinctWithSolution('Solution1', Solution1);
