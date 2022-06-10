d3eb0a201f4202fb6cb70a8cde90931986b392ba
jay7n <lookof@gmail.com>
2022-06-10 13:13:14 +0800

    adds more test cases for 8_9

--- solutions/Recursive_and_Dynamic_Programming/8_9_killedme/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_9_killedme/test.js
@@ -1,6 +1,20 @@
 const testWithSolution = function(title, solution) {
   test(title + 
   'test case 1', () => {
+    const allParentheses = solution(1);
+    const expected = ['()'];
+    expect(allParentheses).toEqual(expect.arrayContaining(expected));
+    expect(expected).toEqual(expect.arrayContaining(allParentheses));
+  });
+  test(title + 
+  'test case 1', () => {
+    const allParentheses = solution(2);
+    const expected = ['()()', '(())'];
+    expect(allParentheses).toEqual(expect.arrayContaining(expected));
+    expect(expected).toEqual(expect.arrayContaining(allParentheses));
+  });
+  test(title + 
+  'test case 3', () => {
     const allParentheses = solution(3);
     const expected = ['((()))', '(()())', '(())()', '()(())', '()()()'];
     expect(allParentheses).toEqual(expect.arrayContaining(expected));
