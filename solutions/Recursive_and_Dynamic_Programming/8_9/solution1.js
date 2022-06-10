e05893384da9be8feb19d0c66c4aee1ce816508b
jay7n <lookof@gmail.com>
2022-06-10 12:45:42 +0800

    implementing 8_9 solution1

--- solutions/Recursive_and_Dynamic_Programming/8_9/solution1.js
+++ solutions/Recursive_and_Dynamic_Programming/8_9/solution1.js
@@ -1,10 +1,39 @@
 module.exports = function Parens(num) {
+  const origNum = num;
+
   const parenSet = {
-    left: 3,
-    right: 3,
+    left: num,
+    right: num,
   };
 
   function recursive(parenSet) {
+    let leftRes = [''];
+    let rightRes = [''];
+    if (parenSet.left > 0) {
+      parenSet.left--;
+      leftRes = recursive(parenSet);
+      leftRes = leftRes.map(paren => {
+        return '(' + paren;
+      });
+      parenSet.left++;
+    }
+    if (parenSet.right > 0) {
+      parenSet.right--;
+      rightRes = recursive(parenSet);
+      rightRes = rightRes.map(paren => {
+        return ')' + paren;
+      });
+    }
 
+    return [].concat(leftRes, rightRes);
   }
+
+  parenSet.left--;
+  let res = recursive(parenSet);
+  res = res.map(paren => {
+    return '(' + paren;
+  });
+
+  console.log('Parens res = ', res);
+  return res;
 }
