d963bb3b28d17dba205e84ac9c5284ae9223db1d
jay7n <lookof@gmail.com>
2022-06-11 17:03:45 +0800

    implementing 8_10 solution1

--- solutions/Recursive_and_Dynamic_Programming/8_10/solution1.js
+++ solutions/Recursive_and_Dynamic_Programming/8_10/solution1.js
@@ -1,3 +1,26 @@
-module.exports = function PaintFill(picArray, x, y) {
+module.exports = function PaintFill(picArray, width, x, y) {
+  const height = picArray.length / width;
 
+  function getValue(x, y) {
+
+  }
+
+  function setValue(x, y, val) {
+
+  }
+
+  function findSurroundings(x, y) {
+
+  }
+
+  function recursive(x, y) {
+    const value = getValue(x, y);
+    setValue(x, y, 'X');
+    const s = findSurroundings(x, y);
+  }
+
+  const res = recursive(x, y);
+
+  console.log('PaintFill res = ', res);
+  return res;
 }
--- solutions/Recursive_and_Dynamic_Programming/8_10/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_10/test.js
@@ -6,22 +6,22 @@ const testWithSolution = function(title, solution) {
       3,1,5,5,
       3,5,1,5,
     ];
-    expect(solution(pic, 1, 1)).toStrictEqual([
+    expect(solution(pic, 4, 1, 1)).toStrictEqual([
       0,5,4,2,
       3,X,5,5,
       3,5,X,5,
     ]);
-    expect(solution(pic, 0, 1)).toStrictEqual([
+    expect(solution(pic, 4, 0, 1)).toStrictEqual([
       0,X,4,2,
       3,1,X,X,
       3,5,1,X,
     ]);
-    expect(solution(pic, 1, 2)).toStrictEqual([
+    expect(solution(pic, 4, 1, 2)).toStrictEqual([
       0,X,4,2,
       3,1,X,X,
       3,5,1,X,
     ]);
-    expect(solution(pic, 0, 3)).toStrictEqual([
+    expect(solution(pic, 4, 0, 3)).toStrictEqual([
       0,5,4,X,
       3,1,5,5,
       3,5,1,5,
