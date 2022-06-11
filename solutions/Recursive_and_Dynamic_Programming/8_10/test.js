bc0a61772519708171755cfefb3cd5215f8964f9
jay7n <lookof@gmail.com>
2022-06-11 18:08:24 +0800

    correct 8_10 testcase

--- solutions/Recursive_and_Dynamic_Programming/8_10/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_10/test.js
@@ -18,16 +18,16 @@ const testWithSolution = function(title, solution) {
       3,1,X,X,
       3,X,1,X,
     ]);
-    // expect(solution(pic, 4, 1, 2)).toStrictEqual([
-    //   0,X,4,2,
-    //   3,1,X,X,
-    //   3,5,1,X,
-    // ]);
-    // expect(solution(pic, 4, 0, 3)).toStrictEqual([
-    //   0,5,4,X,
-    //   3,1,5,5,
-    //   3,5,1,5,
-    // ]);
+    expect(solution(pic.slice(), 4, 2, 1)).toStrictEqual([
+      0,X,4,2,
+      3,1,X,X,
+      3,X,1,X,
+    ]);
+    expect(solution(pic.slice(), 4, 3, 0)).toStrictEqual([
+      0,5,4,X,
+      3,1,5,5,
+      3,5,1,5,
+    ]);
   });
 }
 
