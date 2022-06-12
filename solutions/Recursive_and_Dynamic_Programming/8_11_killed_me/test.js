b5d7edec8c8ab3f3a1b913baceea0306ae7e7438
jay7n <lookof@gmail.com>
2022-06-12 18:29:09 +0800

    added more testcase for 8_11

--- solutions/Recursive_and_Dynamic_Programming/8_11_killed_me/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_11_killed_me/test.js
@@ -1,18 +1,18 @@
 const testWithSolution = function(title, solution) {
-  // test(title + 
-  // 'test case 1', () => {
-  //   const waysOfCents = solution(1);
-  //   const expected = ['1'];
-  //   expect(waysOfCents).toEqual(expect.arrayContaining(expected));
-  //   expect(expected).toEqual(expect.arrayContaining(waysOfCents));
-  // });
-  // test(title + 
-  // 'test case 2', () => {
-  //   const waysOfCents = solution(5);
-  //   const expected = ['1,1,1,1,1', '5'];
-  //   expect(waysOfCents).toEqual(expect.arrayContaining(expected));
-  //   expect(expected).toEqual(expect.arrayContaining(waysOfCents));
-  // });
+  test(title + 
+  'test case 1', () => {
+    const waysOfCents = solution(1);
+    const expected = ['1'];
+    expect(waysOfCents).toEqual(expect.arrayContaining(expected));
+    expect(expected).toEqual(expect.arrayContaining(waysOfCents));
+  });
+  test(title + 
+  'test case 2', () => {
+    const waysOfCents = solution(5);
+    const expected = ['1,1,1,1,1', '5'];
+    expect(waysOfCents).toEqual(expect.arrayContaining(expected));
+    expect(expected).toEqual(expect.arrayContaining(waysOfCents));
+  });
   test(title + 
   'test case 3', () => {
     const waysOfCents = solution(9);
@@ -24,6 +24,32 @@ const testWithSolution = function(title, solution) {
   'test case 4', () => {
     const waysOfCents = solution(12);
     const expected = ['1,1,1,1,1,1,1,1,1,1,1,1', '5,1,1,1,1,1,1,1', '5,5,1,1', '10,1,1'];
+    expect(waysOfCents).toEqual(expect.arrayContaining(expected));
+    expect(expected).toEqual(expect.arrayContaining(waysOfCents));
+  });
+  test(title + 
+  'test case 5', () => {
+    const waysOfCents = solution(30);
+    const expected = [
+        '25,5',
+        '25,1,1,1,1,1',
+        '10,10,5,5',
+        '10,10,5,1,1,1,1,1',
+        '10,10,1,1,1,1,1,1,1,1,1,1',
+        '10,5,5,5,5',
+        '10,5,5,5,1,1,1,1,1',
+        '10,5,5,1,1,1,1,1,1,1,1,1,1',
+        '10,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '10,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '5,5,5,5,5,5',
+        '5,5,5,5,5,1,1,1,1,1',
+        '5,5,5,5,1,1,1,1,1,1,1,1,1,1',
+        '5,5,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '5,5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '5,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1',
+        '10,10,10'
+    ];
     expect(waysOfCents).toEqual(expect.arrayContaining(expected));
     expect(expected).toEqual(expect.arrayContaining(waysOfCents));
   });
