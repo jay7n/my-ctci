a6b78cae37cdb0e752b5c1fd39c2e41bf80aeb32
jay7n <lookof@gmail.com>
2022-06-16 22:34:15 +0800

    added more testcase for 8_12 and implementing solution1

--- solutions/Recursive_and_Dynamic_Programming/8_12/solution1.js
+++ solutions/Recursive_and_Dynamic_Programming/8_12/solution1.js
@@ -1,3 +1,22 @@
-module.exports = function EightQueens() {
+module.exports = function EightQueens(boardSize = 8) {
+  function isUnavailableForGrid(x0, y0, x, y) {
+    if (x < 0 || x > boardSize - 1) {
+      return true;
+    }
+    if (y < 0 || y > boardSize - 1) {
+      return true;
+    }
+    if (x === x0) {
+      return true;
+    }
+    if (y === y0) {
+      return true;
+    }
+    if (Math.abs(y - y0) === Math.abs(x - x0)) {
+      return true;
+    }
+
+    return false;
+  }
 
 }
--- solutions/Recursive_and_Dynamic_Programming/8_12/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_12/test.js
@@ -1,10 +1,24 @@
 const testWithSolution = function(title, solution) {
   test(title + 
   'test case 1', () => {
-
+    const waysOfArranging = solution(1);
+    const X = 'X';
+    const expected = [
+`
+0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0,
+`,
+    ];
+    expect(waysOfArranging).toEqual(expect.arrayContaining(expected));
+    expect(expected).toEqual(expect.arrayContaining(waysOfArranging));
   });
 }
-
 
 const Solution1 = require('./solution1');
 testWithSolution('Solution1', Solution1);
