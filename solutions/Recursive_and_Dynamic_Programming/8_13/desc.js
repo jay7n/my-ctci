780521ee39e6831b2ae91a68cc5c080a6dda91a4
jay7n <lookof@gmail.com>
2022-06-19 21:17:06 +0800

    creating 8_13

---
+++ solutions/Recursive_and_Dynamic_Programming/8_13/desc.js
@@ -0,0 +1,7 @@
+/** 8.13 Stack of Boxes
+ *  You have a stack of n boxes, with widths W1, heights h1 and depths d1. 
+ *  The boxes cannot be rotated and can only be stacked on top of one another 
+ *  if each box in the stack is strictly larger than the box above it in width, height. and depth. 
+ *  Implement a method to compute the height of the tallest possible stack. 
+ *  The height of a stack is the sum of the heights of each box. 
+ */
