8dfd21f6212cee330f7a6ac4e4260eb5acab3d13
jay7n <lookof@gmail.com>
2022-06-20 22:03:52 +0800

    added estcase for 8_13

--- solutions/Recursive_and_Dynamic_Programming/8_13/solution1.js
+++ solutions/Recursive_and_Dynamic_Programming/8_13/solution1.js
@@ -1,3 +1,3 @@
-module.exports = function StackOfBoxes() {
+module.exports = function StackOfBoxes(boxes) {
 
 }
--- solutions/Recursive_and_Dynamic_Programming/8_13/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_13/test.js
@@ -1,4 +1,13 @@
 const testWithSolution = function(title, solution) {
+  const boxes = [
+    [ 1,  2,  3 ], // w, h, d
+    [ 2,  3,  4 ],
+    [ 10, 4,  6 ],
+    [ 4,  7,  2 ],
+  ];
+
+  const possibleTallestHeight = solution(boxes);
+  expect(possibleTallestHeight).toBe();
 }
 
 const Solution1 = require('./solution1');
