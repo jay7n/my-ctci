ba805ae062d3283f59c91a4a732600751ad87be6
jay7n <lookof@gmail.com>
2022-06-21 22:17:29 +0800

    added more testcase for 8_13 and finished solution1

--- solutions/Recursive_and_Dynamic_Programming/8_13/solution1.js
+++ solutions/Recursive_and_Dynamic_Programming/8_13/solution1.js
@@ -1,3 +1,33 @@
 module.exports = function StackOfBoxes(boxes) {
 
+  function gatherPossibleBoxes(baselineBox, boxes) {
+    let possibleBoxes = [];
+    for (const box of boxes) {
+      if (
+        box[0] > baselineBox[0] && 
+        box[1] > baselineBox[1] && 
+        box[2] > baselineBox[2]
+      ) {
+        possibleBoxes.push(box.slice());
+      }
+    }
+    return possibleBoxes;
+  }
+
+  function recursive(baselineBox, boxes) {
+    let tallest = 0;
+    const possibleBoxes = gatherPossibleBoxes(baselineBox, boxes);
+    for (const box of possibleBoxes) {
+      const otherPossibleBoxes = possibleBoxes.filter(b => b !== box);
+      const tallestBeneath = recursive(box, otherPossibleBoxes);
+      tallest = Math.max(tallest, tallestBeneath + box[1]);
+    }
+    return tallest;
+  }
+
+  const tallest = recursive([0,0,0], boxes);
+
+  console.log('StackOfBoxes tallest = ', tallest);
+
+  return tallest;
 }
--- solutions/Recursive_and_Dynamic_Programming/8_13/test.js
+++ solutions/Recursive_and_Dynamic_Programming/8_13/test.js
@@ -1,13 +1,54 @@
 const testWithSolution = function(title, solution) {
-  const boxes = [
-    [ 1,  2,  3 ], // w, h, d
-    [ 2,  3,  4 ],
-    [ 10, 4,  6 ],
-    [ 4,  7,  2 ],
-  ];
+  test(title + 
+  'test case 1', () => {
+    const boxes = [
+      // w, h, d
+      [1, 2, 3],
+    ];
 
-  const possibleTallestHeight = solution(boxes);
-  expect(possibleTallestHeight).toBe();
+    const possibleTallestHeight = solution(boxes);
+    expect(possibleTallestHeight).toBe(2);
+  });
+
+  test(title + 
+  'test case 2', () => {
+    const boxes = [
+      // w, h, d
+      [1, 2, 3],
+      [2, 3, 4],
+    ];
+
+    const possibleTallestHeight = solution(boxes);
+    expect(possibleTallestHeight).toBe(5);
+  });
+
+  test(title + 
+  'test case 3', () => {
+    const boxes = [
+      // w, h, d
+      [1, 2, 3],
+      [2, 3, 4],
+      [10, 4, 6],
+      // [4, 7, 2],
+    ];
+
+    const possibleTallestHeight = solution(boxes);
+    expect(possibleTallestHeight).toBe(9);
+  });
+
+  test(title + 
+  'test case 3', () => {
+    const boxes = [
+      // w, h, d
+      [1, 2, 3],
+      [2, 3, 4],
+      [10, 4, 6],
+      [4, 7, 5],
+    ];
+
+    const possibleTallestHeight = solution(boxes);
+    expect(possibleTallestHeight).toBe(12);
+  });
 }
 
 const Solution1 = require('./solution1');
