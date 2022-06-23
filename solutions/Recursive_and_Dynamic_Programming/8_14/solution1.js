9d171ca1b84bd384dd79ac32dcebd4d91edf24a8
jay7n <lookof@gmail.com>
2022-06-23 21:51:46 +0800

    created 8_14

---
+++ solutions/Recursive_and_Dynamic_Programming/8_14/solution1.js
@@ -0,0 +1,3 @@
+module.exports = function BooleanEvaluation(expression, desiredBool) {
+
+}
---
+++ solutions/Recursive_and_Dynamic_Programming/8_14/test.js
@@ -0,0 +1,15 @@
+const testWithSolution = function(title, solution) {
+  test(title + 
+  'test case 1', () => {
+    const ways = solution('1^0|0|1', false);
+    expect(ways).toBe(2);
+  });
+  test(title + 
+  'test case 2', () => {
+    const ways = solution('0&0&0&1^1|0', true);
+    expect(ways).toBe(10);
+  });
+}
+
+const Solution1 = require('./solution1');
+testWithSolution('Solution1', Solution1);
