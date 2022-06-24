f205d29fb48d4fde21eb6c9f764ef0468e42afe7
jay7n <lookof@gmail.com>
2022-06-24 23:18:51 +0800

    implementing 8_14 solution1

--- solutions/Recursive_and_Dynamic_Programming/8_14/solution1.js
+++ solutions/Recursive_and_Dynamic_Programming/8_14/solution1.js
@@ -1,3 +1,16 @@
 module.exports = function BooleanEvaluation(expression, desiredBool) {
+  function evalSingleExpr(left, op, right) {
+    switch (op) {
+      case '&':
+        return left & right;
+      case '|':
+        return left | right;
+      case '^':
+        return left ^ right;
+    }
+  }
 
+  function evalExpression(expr) {
+
+  }
 }
