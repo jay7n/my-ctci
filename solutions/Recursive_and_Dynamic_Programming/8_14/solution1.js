39a1d8c4986035b3e962f22dae0e38f66c8591c4
jay7n <lookof@gmail.com>
2022-06-25 23:50:26 +0800

    implementing 8_14 solution1

--- solutions/Recursive_and_Dynamic_Programming/8_14/solution1.js
+++ solutions/Recursive_and_Dynamic_Programming/8_14/solution1.js
@@ -11,6 +11,17 @@ module.exports = function BooleanEvaluation(expression, desiredBool) {
   }
 
   function evalExpression(expr) {
+    exprArr = expr.split('');
+    let left, op, right;
+    for (let i = 0; i < exprArr.length; i+=3) {
+      if (left) {
 
+      } else {
+        left = exprArr[i];
+        op = exprArr[i + 1];
+        right = exprArr[i + 2];
+        left = evalExpression(left, op, right);
+      }
+    }
   }
 }
