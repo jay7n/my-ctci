fa0a31ec2592b2dfb1614b662aaa9872865db106
jay7n <lookof@gmail.com>
2022-07-02 23:28:29 +0800

    implementing 8_14 solution1

--- solutions/Recursive_and_Dynamic_Programming/8_14/solution1.js
+++ solutions/Recursive_and_Dynamic_Programming/8_14/solution1.js
@@ -1,4 +1,4 @@
-const { evalExpr } = require('./helpers');
+const { evalComplexExpr } = require('./helpers');
 module.exports = function BooleanEvaluation(expression, desiredBool) {
   const result = evalExpr(expression);
   console.log('result of expression "', expression, '" is ', result);
