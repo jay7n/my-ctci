432ffa8816f5aa11b0de09a02fd1b4eebe681663
jay7n <lookof@gmail.com>
2022-07-07 23:36:20 +0800

    creating 10_1

--- solutions/Sorting_and_Searching/10_1/test.js
+++ solutions/Sorting_and_Searching/10_1/test.js
@@ -1,5 +1,6 @@
 const testWithSolution = function(title, solution) {
-
+  const arr1 = [1, 5, 8, 9, 12, 15, 17];
+  const arr2 = [2, 3, 6, 10, 11, 14, 20];
 }
 
 const Solution1 = require('./solution1');
