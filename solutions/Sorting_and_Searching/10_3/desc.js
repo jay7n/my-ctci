719b2be3568d72a41886de1d17c446f3621567bf
jay7n <lookof@gmail.com>
2022-07-19 23:11:04 +0800

    creating 10_3 and 10_4

--- solutions/Sorting_and_Searching/10_3/desc.js
+++ solutions/Sorting_and_Searching/10_3/desc.js
@@ -1,9 +1,9 @@
-/** 10.4 Sorted Search, No Size
- * You are given an array-like data structure Listy which lacks a size method. 
- * It does, however, have an elementAt (i) method that returns the element at index i in 0(1) time. 
- * If i is beyond the bounds of the data structure, it returns -1. 
- * (For this reason, the data structure only supports positive integers.) 
- * Given a Listy which contains sorted, positive integers, 
- * find the index at which an element x occurs. 
- * If x occurs multiple times, you may return any index. 
+/** 10.3 Search in Rotated Array
+ * Given a sorted array of n integers that has been rotated an unknown number of times, 
+ * write code to find an element in the array. 
+ * You may assume that the array was originally sorted in increasing order. 
+ * 
+ * EXAMPLE 
+ *  Input:find 5 in {15, 16, 19, 20, 25, 1, 3, 4, 5, 7, 10, 14} 
+ *  Output: 8 (the index of 5 in the array) 
  */
