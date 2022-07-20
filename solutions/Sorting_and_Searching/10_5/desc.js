35ba143cf31bbde3e734ba2028029422870fb4b6
jay7n <lookof@gmail.com>
2022-07-20 21:45:39 +0800

    creating 10_5

---
+++ solutions/Sorting_and_Searching/10_5/desc.js
@@ -0,0 +1,8 @@
+/** 10.5 Sparse Search
+ *  Given a sorted array of strings that is interspersed with empty strings, 
+ *  write a method to find the location of a given string. 
+ * 
+ *  EXAMPLE 
+ *    Input: ball, {"at", "", "", "ball", "", "", "car", "", "", "dad", "", ""}
+ *    Output: 4 
+ */
